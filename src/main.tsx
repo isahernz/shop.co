a142d7056907f0dd8ca3ec3a5bc08ca5b58404e8
Brandon Hernández <isahecam@gmail.com>
2025-02-08 20:55:06 -0600

    feat: implement routing for product details

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.tsx";
 import "./styles/globals.css";
+import { ProductDetail } from "./pages/product-detail/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
