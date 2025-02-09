fbbbfe3964e2cc804927e99f175aa96f126551b7
Brandon Hernández <isahecam@gmail.com>
2025-02-08 20:55:27 -0600

    feat: add custom useFetch hook for data fetching

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(
+          err instanceof Error ? error : new Error("Upps! Ocurred an error")
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { data, error, loading };
+};
