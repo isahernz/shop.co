312b5e20ef744e38dd6bfea0af09b2ecd11298d2
Brandon Hernández <isahecam@gmail.com>
2025-02-09 17:44:58 -0600

    feat(new-arrivals): implement overflow handling

--- src/components/NewArrivals/index.tsx
+++ src/components/NewArrivals/index.tsx
@@ -9,18 +9,21 @@ export const NewArrivals = () => {
     loading,
   } = useFetch<ProductProps[]>("https://fakestoreapi.com/products?limit=4");
 
-  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <section className="px-4 mt-10">
-      <h2 className="text-3xl text-center mb-8 font-bold uppercase">
+    <section className="max-w-7xl mx-auto mt-12 md:mt-14 lg:mt-16 px-4 ">
+      <h2 className="text-3xl md:text-4xl lg:text-5xl text-center mb-8 md:mb-11 lg:mb-14 font-bold uppercase font-bowlby tracking-wider">
         New Arrivals
       </h2>
-      <div className="flex gap-x-4 overflow-x-auto mb-3">
-        {products?.map((product: ProductProps) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
+      <div className="relative flex w-full snap-x snap-mandatory gap-x-4 overflow-x-auto">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          products?.map((product: ProductProps) => (
+            <ProductCard key={product.id} {...product} />
+          ))
+        )}
       </div>
     </section>
   );
