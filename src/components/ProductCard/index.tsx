22dce0dd08e8c5ea87814b98d92e8e196400f7bf
Brandon Hernández <isahecam@gmail.com>
2025-02-09 16:28:51 -0600

    fix(product-card): resolve styling and layout issues

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -2,19 +2,24 @@ import { ProductProps } from "../../types/product";
 
 export const ProductCard = ({ id, title, price, image }: ProductProps) => {
   return (
-    <article className="w-52">
-      <figure className="size-52 rounded-3xl bg-ivory-mist grid place-content-center">
+    <article className="w-full max-w-56 shrink-0">
+      <figure className="w-full h-auto mb-3 rounded-3xl bg-ivory-mist grid place-content-center aspect-square">
         <img
           src={image}
           alt={title}
-          className="size-40 object-cover mix-blend-multiply"
+          className="size-40 mix-blend-multiply object-contain "
         />
       </figure>
       <div className="flex flex-col gap-y-1">
-        <a href={`/product/${id}`} className="text-base font-bold text-black">
+        <a
+          href={`/product/${id}`}
+          className="text-baseb text-pretty font-bold text-black"
+        >
           {title}
         </a>
-        <p className="text-xl text-black font-bold">${price}</p>
+        <p className="text-xl text-black font-bold slashed-zero tabular-nums">
+          ${price}
+        </p>
       </div>
     </article>
   );
