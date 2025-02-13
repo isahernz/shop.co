6e00771fe37d545d7607d5f6a1d52d4e1aa206e3
Brandon Hernández <isahecam@gmail.com>
2025-02-12 18:55:32 -0600

    feat(store-highlight): enhance text styles

--- src/components/StoreHighlight/index.tsx
+++ src/components/StoreHighlight/index.tsx
@@ -3,8 +3,10 @@ import { HighlightProps } from "../../types/highlight";
 export const StoreHighlight = ({ title, description }: HighlightProps) => {
   return (
     <article className="max-w-max nth-[3n+1]:border-r nth-[3n+1]:pr-8">
-      <h3 className="text-2xl font-bold">{title}</h3>
-      <p className="text-xs text-black/60 text-nowrap">{description}</p>
+      <h3 className="text-2xl font-bold md:text-[40px]">{title}</h3>
+      <p className="text-xs text-black/60 text-nowrap md:text-base">
+        {description}
+      </p>
     </article>
   );
 };
