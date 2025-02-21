ba4e853cf1410f3d727faeaabd32a37686f18a34
Brandon Hernández <isahecam@gmail.com>
2025-02-21 17:12:24 -0600

    feat(add-to-cart-button): add handleOnClick prop to AddToCartButton component

--- src/components/AddToCartButton/index.tsx
+++ src/components/AddToCartButton/index.tsx
@@ -1,10 +1,18 @@
 interface AddToCartButtonProps {
   label?: string;
   children?: React.ReactNode;
+  handleOnClick: () => void;
 }
-export const AddToCartButton = ({ label, children }: AddToCartButtonProps) => {
+export const AddToCartButton = ({
+  label,
+  children,
+  handleOnClick,
+}: AddToCartButtonProps) => {
   return (
-    <button className="w-full flex-grow h-12 py-2 inline-flex justify-center items-center rounded-full font-medium bg-black text-white cursor-pointer">
+    <button
+      className="w-full flex-grow h-12 py-2 inline-flex justify-center items-center rounded-full font-medium bg-black text-white cursor-pointer"
+      onClick={handleOnClick}
+    >
       {children || label}
     </button>
   );
