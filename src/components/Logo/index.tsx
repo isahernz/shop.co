f05c58972d743b2ab13c2eb921a50d49b0f7b387
Brandon Hernández <isahecam@gmail.com>
2025-02-21 17:13:31 -0600

    feat(logo): replace anchor tag with Link component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router";
 import shopLogo from "../../assets/images/shop.co.svg";
 
 export const Logo = () => {
   return (
-    <a href="/">
+    <Link to="/">
       <img
         src={shopLogo}
         alt="Shop.co - Online Shopping"
         className="w-32 h-full"
       />
-    </a>
+    </Link>
   );
 };
