bb43c0638d023c9d3fd17509b2eed083784f9df6
Brandon Hernández <isahecam@gmail.com>
2025-02-21 10:59:42 -0600

    feat(product-details-content): create ProductDetailsContent component

---
+++ src/components/ProductDetailsContent/index.tsx
@@ -0,0 +1,45 @@
+import { ProductQuantitySelector } from "../../components/ProductQuantitySelector";
+import { AddToCartButton } from "../../components/AddToCartButton";
+import { useFetch } from "../../hooks/useFetch";
+import { ProductProps } from "../../types/product";
+
+export const ProductDetailsContent = ({ id }: { id: string | undefined }) => {
+  console.log("Content reendered");
+  const {
+    data: product,
+    error,
+    loading,
+  } = useFetch<ProductProps>(`https://fakestoreapi.com/products/${id}`);
+
+  if (loading) return <p>Loading product...</p>;
+  if (error) return <p>Error</p>;
+
+  return (
+    <section className="flex flex-col gap-y-6 sm:flex-row sm:gap-x-6 max-w-6xl mx-auto">
+      <figure className="w-full h-[290px] sm:w-[290px] sm:h-auto rounded-3xl bg-ivory-mist grid place-content-center shrink-0">
+        <img
+          src={product?.image}
+          alt={product?.title}
+          className="size-40 mix-blend-multiply object-contain "
+        />
+      </figure>
+      <div className="flex flex-col gap-y-6">
+        <div>
+          <h1 className="text-2xl font-bold uppercase font-bowlby tracking-wider mb-5">
+            {product?.title}
+          </h1>
+          <p className="text-xl text-black font-bold slashed-zero tabular-nums mb-3">
+            ${product?.price}
+          </p>
+          <p className="text-sm text-black/60 text-pretty">
+            {product?.description}
+          </p>
+        </div>
+        <div className="flex items-center gap-3">
+          <ProductQuantitySelector />
+          <AddToCartButton label="Add to Cart" />
+        </div>
+      </div>
+    </section>
+  );
+};
