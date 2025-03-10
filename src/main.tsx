3644a6fb5a4f166280571e34ad126ce3bc9f4cac
Brandon Hernández <isahecam@gmail.com>
2025-03-10 13:49:17 -0600

    feat(routes): add ShoppingCartPage route

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { ProductDetailsPage } from "./pages/ProductDetailsPage/index.tsx";
+import { ShoppingCartPage } from "./pages/ShoppingCartPage/index.tsx";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext.tsx";
 import App from "./App.tsx";
 import "./styles/globals.css";
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/product/:id" element={<ProductDetailsPage />} />
+          <Route path="/shopping-cart" element={<ShoppingCartPage />} />
         </Routes>
       </BrowserRouter>
     </StrictMode>
