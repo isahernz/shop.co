2d490572d6fa69c2ae694a0b6180fd06ea1dd44c
Brandon Hernández <isahecam@gmail.com>
2025-03-10 13:55:47 -0600

    feat(Navbar): update shopping cart badge

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,14 @@
 import { ShoppingCart } from "lucide-react";
-import { useContext } from "react";
-import { ShoppingCartContext } from "../../context/ShoppingCartContext";
+import { useShoppingCart } from "../../hooks/useShoppingCart";
 import { Logo } from "../Logo";
 import { NavLink } from "../NavLink";
 
 export const Navbar = () => {
-  const context = useContext(ShoppingCartContext);
+  const { totalQuantity } = useShoppingCart();
 
   return (
     <header>
-      <nav className="max-w-6xl mx-auto flex items-center justify-between py-5 px-4 lg:px-0">
+      <nav className="max-w-6xl mx-auto flex items-center justify-between py-5 px-4 border-b border-black/10 lg:px-0">
         <Logo />
         <ul>
           <li className="relative">
@@ -17,7 +16,7 @@ export const Navbar = () => {
               <ShoppingCart />
             </NavLink>
             <span className="size-5 grid place-content-center absolute rounded-full bg-red-400 text-xs text-white px-2 -top-2 -right-2.5">
-              {context.count}
+              {totalQuantity}
             </span>
           </li>
         </ul>
