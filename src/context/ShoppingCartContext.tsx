a919f6c8510c113f7f68fb3cdf70381c31c993cf
Brandon Hernández <isahecam@gmail.com>
2025-03-10 13:56:56 -0600

    feat(ShoppingCartContext): refactor context to manage total quantity and add product to cart

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -2,10 +2,11 @@ import { createContext, useState } from "react";
 import { ProductProps } from "../types/product";
 
 interface ShoppinCartContextType {
-  count: number;
-  setCount: React.Dispatch<React.SetStateAction<number>>;
+  totalQuantity: number;
+  setTotalQuantity: React.Dispatch<React.SetStateAction<number>>;
   cartItems: ProductProps[];
   setCartItems: React.Dispatch<React.SetStateAction<ProductProps[]>>;
+  addToCart: (product: ProductProps) => void;
 }
 
 interface ShoppinCartProviderProps {
@@ -19,12 +20,64 @@ export const ShoppingCartContext = createContext<ShoppinCartContextType>(
 export const ShoppingCartProvider = ({
   children,
 }: ShoppinCartProviderProps) => {
-  const [count, setCount] = useState<number>(0);
+  // Total items in cart
+  const [totalQuantity, setTotalQuantity] = useState<number>(0);
+
+  // Cart items
   const [cartItems, setCartItems] = useState<ProductProps[]>([]);
+
+  const addToCart = (product: ProductProps) => {
+    setTotalQuantity(totalQuantity + 1);
+
+    // Check if product already exists in cart
+    // If it isn't, return -1
+    const productPositionInCart = cartItems.findIndex(
+      (item) => item.id === product.id
+    );
+    console.log(productPositionInCart);
+
+    if (productPositionInCart !== -1) {
+      const newCartItems = structuredClone(cartItems);
+      newCartItems[productPositionInCart].quantity += 1;
+      console.log(newCartItems);
+      return setCartItems(newCartItems);
+    }
+
+    setCartItems([
+      ...cartItems,
+      {
+        ...product,
+        quantity: 1,
+      },
+    ]);
+  };
+
+  // const removeFromCart = (product: ProductProps) => {
+  //   setCount(count - product.quantity);
+  //   setCartItems((prevState) =>
+  //     prevState.filter((cartItem) => cartItem.id !== product.id)
+  //   );
+  // };
+
+  // const clearCart = () => {
+  //   setCount(0);
+  //   setCartItems([]);
+  // };
+
+  // const getQuantityProduct = (id: number) => {
+  //   const product = cartItems.find((item) => item.id === id);
+  //   return product?.quantity || 0;
+  // };
 
   return (
     <ShoppingCartContext.Provider
-      value={{ count, setCount, cartItems, setCartItems }}
+      value={{
+        totalQuantity,
+        setTotalQuantity,
+        cartItems,
+        setCartItems,
+        addToCart,
+      }}
     >
       {children}
     </ShoppingCartContext.Provider>
