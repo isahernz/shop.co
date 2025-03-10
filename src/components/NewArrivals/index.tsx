eb0c91108641d9a5079f37676c69ad5f68d2d201
Brandon Hernández <isahecam@gmail.com>
2025-03-10 13:55:26 -0600

    feat(NewArrivals): add ActionLink to view all products

--- src/components/NewArrivals/index.tsx
+++ src/components/NewArrivals/index.tsx
@@ -1,5 +1,6 @@
 import { useFetch } from "../../hooks/useFetch";
 import { ProductProps } from "../../types/product";
+import { ActionLink } from "../ActionLink";
 import { ProductCard } from "../ProductCard";
 
 export const NewArrivals = () => {
@@ -12,11 +13,11 @@ export const NewArrivals = () => {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <section className="max-w-7xl mx-auto mt-12 md:mt-14 lg:mt-16 px-4 ">
-      <h2 className="text-3xl md:text-4xl lg:text-5xl text-center mb-8 md:mb-11 lg:mb-14 font-bold uppercase font-bowlby tracking-wider">
+    <section className="max-w-7xl mx-auto mt-12 md:mt-14 lg:mt-16 px-4 text-center">
+      <h2 className="text-3xl md:text-4xl lg:text-5xl mb-8 md:mb-11 lg:mb-14 font-bold uppercase font-bowlby tracking-wider">
         New Arrivals
       </h2>
-      <div className="relative flex w-full snap-x snap-mandatory gap-x-4 overflow-x-auto">
+      <div className="relative flex w-full snap-x snap-mandatory gap-x-4 overflow-x-auto mb-8 hide-scrollbar">
         {loading ? (
           <p>Loading...</p>
         ) : (
@@ -25,6 +26,11 @@ export const NewArrivals = () => {
           ))
         )}
       </div>
+      <ActionLink
+        label="View All"
+        route="all-products"
+        className="border border-black/10 transition-all hover:scale-110"
+      />
     </section>
   );
 };
